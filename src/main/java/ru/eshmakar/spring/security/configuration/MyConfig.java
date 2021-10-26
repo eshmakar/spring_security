dbf470cd43306cd6a9a681bb46e170e29d54de46
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-10-26 21:57:41 +0300

    Данные юзеров хранятся в базе данных

--- src/main/java/ru/eshmakar/spring/security/configuration/MyConfig.java
+++ src/main/java/ru/eshmakar/spring/security/configuration/MyConfig.java
@@ -1,5 +1,6 @@
 package ru.eshmakar.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,11 +8,28 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
 //вместо web.xml будем использовать конфигурацию из java класса
 @Configuration
 @ComponentScan(basePackages = "ru.eshmakar.spring.security")
 @EnableWebMvc
 public class MyConfig {
+
+    @Bean
+    public DataSource dataSource() {//Spring будет автоматический соединяться к базу данных
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("root");
+            dataSource.setPassword("root");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
 
     @Bean
     public ViewResolver viewResolver(){//чтобы использовать jsp страницы для отображения
--- src/main/java/ru/eshmakar/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/ru/eshmakar/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.eshmakar.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,17 +8,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity // эта аннотация уже содержить аннотацию @Configuration, поэтому написать еще раз нет смысла
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {//перезаписываем только этот метод
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();// UserBuilder - Создает добавляемого пользователя. withDefaultPasswordEncoder() - Создает пользователя и автоматически кодирует предоставленный пароль с помощью PasswordEncoderFactories.createDelegatingPasswordEncoder()
+        auth.jdbcAuthentication().dataSource(dataSource);//с этим объясняем Spring-у, что необходимо брать информацию о юзерах из базы данных (настройки подлкючения находятся в dataSource)
 
-        auth.inMemoryAuthentication()//данные юзеров и паролей будут хранятся в памяти
-                .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
-                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+        //ДАННЫЕ ЮЗЕРОВ УЖЕ БУДУТ ХРАНЯТСЯ В БАЗЕ ДАННЫХ
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();// UserBuilder - Создает добавляемого пользователя. withDefaultPasswordEncoder() - Создает пользователя и автоматически кодирует предоставленный пароль с помощью PasswordEncoderFactories.createDelegatingPasswordEncoder()
+//
+//        auth.inMemoryAuthentication()//данные юзеров и паролей будут хранятся в памяти
+//                .withUser(userBuilder.username("zaur").password("zaur").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+//                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
 
     }
 
